bfaf46f9453cc7bfd0401440625f04f838fe45b2
Atkn0 <a.atakantosun@gmail.com>
2022-11-15 23:49:28 +0300

    last ui

---
+++ app/src/main/java/com/example/kotlinnewchatapp/Adapters/ChatFragmentRecyclerViewAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinnewchatapp.Adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlinnewchatapp.databinding.MessageChatLayoutBinding
+
+class ChatFragmentRecyclerViewAdapter : RecyclerView.Adapter<ChatFragmentRecyclerViewAdapter.myHolder>() {
+    class myHolder(val binding:MessageChatLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myHolder {
+        val binding = MessageChatLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return  myHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: myHolder, position: Int) {
+        holder.binding.chatBoxLayout
+    }
+
+    override fun getItemCount(): Int {
+        return 5
+    }
+}
